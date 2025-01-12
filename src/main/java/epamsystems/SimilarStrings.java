35cd990c3746c057101cc50f02d78cf035bcc598
Divya Gupta <divygupta0319@gmail.com>
2025-01-12 18:07:08 +0530

    Added EPAM SimilarStrings problem solution

--- src/main/java/epamsystems/SimilarStrings.java
+++ src/main/java/epamsystems/SimilarStrings.java
@@ -1,5 +1,10 @@
 package epamsystems;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 // write a boolean method that checks if two strings in parameter are similar.
 /*
 1- abcba, abc ----> similar
@@ -13,6 +18,29 @@ package epamsystems;
 public class SimilarStrings {
 
     public static void main(String[] args) {
-
+        System.out.println(areSimilar("abcba", "abc"));        // true
+        System.out.println(areSimilar("bca", "abcbcbc"));     // true
+        System.out.println(areSimilar("bcd", "bc"));           // false
+        System.out.println(areSimilar("abcd", "abde"));        // false
+        System.out.println(areSimilar("bc", "abcd"));          // false
+        System.out.println(areSimilar("bccc", "bbbbbbc"));     // true
     }
+
+    public static boolean areSimilar(String first, String second) {
+        Set<Character> set1 = toCharacterSet(first);
+        Set<Character> set2 = toCharacterSet(second);
+
+        return set1.equals(set2);
+    }
+
+    private static Set<Character> toCharacterSet(String str) {
+        Set<Character> set = new HashSet<>();
+        for(char ch : str.toCharArray()) {
+            set.add(ch);
+        }
+
+        return set;
+    }
+
+
 }
